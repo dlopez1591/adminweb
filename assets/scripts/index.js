bd6f5e88dcf4231a29d8d36cad203cec54d60896
dlopez1591 <dlopez1591@gmail.com>
2023-09-11 21:16:12 -0500

    todos los frameworks instalados y funcionando

---
+++ assets/scripts/index.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+     //declaracion de variables
+     message: "hello vue!"
+    }
+  },
+  methods: {
+    //metodos
+  }
+}).mount('#app')
