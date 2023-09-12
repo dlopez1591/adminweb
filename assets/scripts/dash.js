70ee32aab6853c1ea3319abfeeff196baa68fce0
dlopez1591 <dlopez1591@gmail.com>
2023-09-12 13:36:53 -0500

    creado navbar

---
+++ assets/scripts/dash.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+     //declaracion de variables
+     nombre: "Daniel!"
+    }
+  },
+  methods: {
+    //metodos
+  }
+}).mount('#app')
